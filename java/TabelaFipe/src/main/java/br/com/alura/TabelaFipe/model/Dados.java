5771cd4b93ddebd31fe5fc7e810f788736e52494
Regiane Piccinin <63248452+repiccinin@users.noreply.github.com>
2024-05-10 10:23:15 -0300

    Tabela Fipe completo

---
+++ java/TabelaFipe/src/main/java/br/com/alura/TabelaFipe/model/Dados.java
@@ -0,0 +1,10 @@
+package br.com.alura.TabelaFipe.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record Dados (@JsonAlias("codigo") String codigo,
+                        @JsonAlias("nome") String nome) {
+}
+
